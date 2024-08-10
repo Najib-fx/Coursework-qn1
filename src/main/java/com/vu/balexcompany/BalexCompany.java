bc7787f3c47cda892a69004285531507acf3a427
Najib <61076460+Najib-fx@users.noreply.github.com>
2024-08-10 04:33:51 +0300

    Add files via upload

---
+++ src/main/java/com/vu/balexcompany/BalexCompany.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.vu.balexcompany;
+
+/**
+ *
+ * @author HP
+ */
+public class BalexCompany {
+
+    public static void main(String[] args) {
+        balexCalculations balexCalculations = new balexCalculations();
+        //Employee 1
+        balexCalculations.calculatePay(7.50,35);
+        //Employee 2
+        balexCalculations.calculatePay(8.20,47);
+        //Employee 3
+        balexCalculations.calculatePay(10.00,73);
+    }
+}
